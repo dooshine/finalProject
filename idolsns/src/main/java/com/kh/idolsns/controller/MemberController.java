0d608f5517ae61fbc498d2f5bdc319bcb0ff276c
MYEHUB0 <algpalwnwkd@naver.com>
2023-05-08 19:22:03 +0900

    dhdP~

---
+++ idolsns/src/main/java/com/kh/idolsns/controller/MemberController.java
@@ -0,0 +1,5 @@
+package com.kh.idolsns.controller;
+
+public class MemberController {
+
+}
---
+++ idolsns/src/main/java/com/kh/idolsns/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package com.kh.idolsns.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+	private String memberId;
+	private String memberPw;
+	private String memberNick;
+	private int memberPoint;
+	priva
+}
