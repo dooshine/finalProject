b5176382f063b579015cb5e0e2d74d40ebe28798
importqkk <import_qkk@naver.com>
2023-05-10 18:07:25 +0900

    채팅방 전송, 읽음 관련 레파지토리

---
+++ idolsns/src/main/java/com/kh/idolsns/repo/ChatReadRepo.java
@@ -0,0 +1,9 @@
+package com.kh.idolsns.repo;
+import com.kh.idolsns.dto.ChatReadDto;
+
+public interface ChatReadRepo {
+
+	void saveMessage(ChatReadDto dto);
+	void updateReadTime(int chatMessageNo, String chatReciever);
+	
+}
