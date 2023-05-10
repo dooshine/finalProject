3faa5be1199f19c1f6681744f48af3f5df0870c0
importqkk <import_qkk@naver.com>
2023-05-10 18:08:33 +0900

    채팅 컨트롤러(테스트용)

---
+++ idolsns/src/main/java/com/kh/idolsns/controller/ChatTestController.java
@@ -0,0 +1,27 @@
+package com.kh.idolsns.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.kh.idolsns.repo.ChatRoomRepo;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatTestController {
+	
+	@Autowired
+	private ChatRoomRepo chatRoomRepo;
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("chatRoomList", chatRoomRepo.listRoom());
+		return "chat/main";
+	}
+	
+	@GetMapping("/chatRoomNo")
+	public String chatRooms() {
+		return "chat/chatRoom";
+	}
+	
+}
