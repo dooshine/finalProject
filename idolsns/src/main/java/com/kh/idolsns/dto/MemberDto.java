99d788b4425074aac17ad3cd289a3cba5f454ed3
MYEHUB0 <algpalwnwkd@naver.com>
2023-05-13 13:41:26 +0900

    .

---
+++ idolsns/src/main/java/com/kh/idolsns/dto/MemberDto.java
@@ -0,0 +1,21 @@
+package com.kh.idolsns.dto;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers.DateDeserializer;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+	private String memberId;
+	private String memberPw;
+	private String memberNick;
+	private int memberPoint;
+	private String memberEmail;
+	private String memberAgree;
+	private Date memberJoin;
+	private String memberLevel;
+	private Date memberLogin;
+}
---
+++ idolsns/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!-- 로그인 -->
+<form class="form" action="login" method="post">
+	아이디 : <input type="text" name="memberId" >
+	비밀번호 : <input type="password" name="memberPw">
+	
+</form>
