1d2dad34ee40a1acc780da74e30414f5b0abb6bb
dooshine <doyungi0304@gmail.com>
2023-05-19 09:46:18 +0900

    fix

--- idolsns/src/main/java/com/kh/idolsns/controller/ArtistController.java
+++ idolsns/src/main/java/com/kh/idolsns/controller/ArtistController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/artist")
 public class ArtistController {
     
-    @GetMapping("/${artistName}")
+    @GetMapping("/{artistName}")
     public String home(@PathVariable String artistName){
         System.out.println("artistName: " + artistName);
         return "/artist/home";
