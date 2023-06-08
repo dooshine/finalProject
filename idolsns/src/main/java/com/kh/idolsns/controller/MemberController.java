c809afa4d6f21e10e7a4ca02013b0b6a50b37ff7
MYEHUB0 <algpalwnwkd@naver.com>
2023-06-08 13:02:10 +0900

    그냥 하기 싫어서 한번 올리기..

--- idolsns/src/main/java/com/kh/idolsns/controller/MemberController.java
+++ idolsns/src/main/java/com/kh/idolsns/controller/MemberController.java
@@ -168,6 +168,14 @@ public class MemberController {
 		return "member/mypage";
 	}
 	
+	//내페이지 or 남의페이지 구분
+	@GetMapping("/mypage")
+	@ResponseBody
+	public String mypage(HttpSession session) {
+		String memberId = (String) session.getAttribute("memberId");
+		return memberId;
+	}
+	
 	//마이페이지 - 아이디, 닉네임 조회
 	@GetMapping("/profile")
 	@ResponseBody
