a36631e39e78f9cc0dfe3c98584794d29a7042d0
dooshine <doyungi0304@gmail.com>
2023-06-12 14:00:43 +0900

    로딩 화면 추가 @CrossOrigin 추가

--- idolsns/src/main/java/com/kh/idolsns/restcontroller/CalendarRestController.java
+++ idolsns/src/main/java/com/kh/idolsns/restcontroller/CalendarRestController.java
@@ -1,6 +1,7 @@
 package com.kh.idolsns.restcontroller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kh.idolsns.dto.CalendarDto;
 import com.kh.idolsns.repo.CalendarRepo;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/calendar")
 public class CalendarRestController {
